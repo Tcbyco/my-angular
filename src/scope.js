697dc10b2c05db3bc5d686c04986d16a30cfbb1d
Toby Coy <coy.toby@gmail.com>
2020-09-11 17:05:08 +1000

    scopes: check for dirty values. init watch vals

--- src/scope.js
+++ src/scope.js
@@ -5,18 +5,36 @@ function Scope() {
   this.$$watchers = [];
 }
 
+// This fn definition serves as a unique initial value for a watched value.
+// It is intentionally blank, as the function is never run.
+// For test case, see scope_spec:"still calls listener on
+// first digest when watch value is undefined"
+function initWatchVal() {}
+
 Scope.prototype.$watch = function (watchFn, listenerFn) {
   var watcher = {
     watchFn: watchFn,
     listenerFn: listenerFn,
+    last: initWatchVal,
   };
 
   this.$$watchers.push(watcher);
 };
 
+// arrow function used so 'this' will refer to the instance instead of window
 Scope.prototype.$digest = function () {
-  this.$$watchers.forEach(function (watcher) {
-    watcher.listenerFn();
+  var newValue, oldValue;
+  this.$$watchers.forEach((watcher) => {
+    newValue = watcher.watchFn(this);
+    oldValue = watcher.last;
+    if (newValue !== oldValue) {
+      watcher.last = newValue;
+      watcher.listenerFn(
+        newValue,
+        oldValue === initWatchVal ? newValue : oldValue,
+        this
+      );
+    }
   });
 };
 
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -21,5 +21,65 @@ describe("Scope", function () {
       scope.$digest();
       expect(listenerFn).toHaveBeenCalled();
     });
+    it("calls the watch function with the scope as the argument.", function () {
+      var watchFn = jasmine.createSpy();
+      var listenerFn = function () {};
+      scope.$watch(watchFn, listenerFn);
+      scope.$digest();
+      expect(watchFn).toHaveBeenCalledWith(scope);
+    });
+    it("calls the listener function when the watched value changes.", function () {
+      scope.someValue = "a";
+      scope.counter = 0;
+      scope.$watch(
+        function (scope) {
+          return scope.someValue;
+        },
+        function (newValue, oldValue, scope) {
+          scope.counter++;
+        }
+      );
+      expect(scope.counter).toBe(0);
+      scope.$digest();
+      // first call to digest should always run the listener function
+      expect(scope.counter).toBe(1);
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+      scope.someValue = "b";
+      expect(scope.counter).toBe(1);
+      // Now that the watch function returns a different value,
+      // the listener function will run during $digest();
+      scope.$digest();
+      expect(scope.counter).toBe(2);
+    });
+    it("still calls listener on first digest() when watch value is undefined", function () {
+      scope.counter = 0;
+      scope.$watch(
+        function (scope) {
+          return scope.someValue; // undefined
+        },
+        function (newValue, oldValue, scope) {
+          scope.counter++;
+        }
+      );
+      scope.$digest();
+      // still runs listener
+      expect(scope.counter).toBe(1);
+    });
+    it("calls listener with new value instead of old/initial value, the first time only", function () {
+      scope.someValue = 123;
+      var oldValueGiven;
+      scope.$watch(
+        function (scope) {
+          return scope.someValue;
+        },
+        // set oldValueGiven to be whatever oldValue was passed to listener
+        function (newValue, oldValue, scope) {
+          oldValueGiven = oldValue;
+        }
+      );
+      scope.$digest();
+      expect(oldValueGiven).toBe(123);
+    });
   });
 });
